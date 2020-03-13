7357eea4d36420158b576355e1d0bbd96de3e9a4
Joel Hobgood <jhobgoo@gmail.com>
2020-03-13 02:37:28 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,13 +1,14 @@
 # Your code goes here!
 class Anagram
   
-attr_accessor :word
+  attr_accessor :word
   
-def initialize(word)
-  @word = word
-end
+  def initialize(word)
+    @word = word
+  end
 
-def match(words)
-  @word.match(%w(array))
+  def match(words)
+    @word.match(%w(array))
+  end
 
 end
